22592cac3c74c06c0a6a08b7f0735d335cf2c7e7
Luann244 <weversonluan17@gmail.com>
2021-07-24 01:44:10 -0300

    alteraçao no arquivo _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-export default MyApp
+
+export default MyApp;
